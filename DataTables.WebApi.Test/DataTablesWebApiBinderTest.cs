a9135d01c437fc9663100327e03db1a293416ac0
Tim VanFosson <tvanfosson@gmail.com>
2014-11-18 23:37:49 -0600

    Add initial test for WebApi query string binder

---
+++ DataTables.WebApi.Test/DataTablesWebApiBinderTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+using System.Web.Http.Metadata.Providers;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.Routing;
+using DataTables.Core;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataTables.WebApi.Test
+{
+    [TestClass]
+    public class DataTablesWebApiBinderTest
+    {
+
+        private DataTablesWebApiBinderTestContext _c;
+
+        [TestMethod]
+        public void When_a_request_with_query_parameters_is_bound_the_result_is_successful()
+        {
+            var dataRequest = _c.GetValidRequest();
+
+            var binder = _c.GetBinder(dataRequest, true);
+
+            var bindingContext = _c.GetBindingContext();
+
+            var result = binder.BindModel(_c.ActionContext, bindingContext);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            _c = new DataTablesWebApiBinderTestContext();
+        }
+
+        private class DataTablesWebApiBinderTestContext
+        {
+            public HttpActionContext ActionContext;
+
+            public DataTablesWebApiBinder<T> GetBinder<T>(T data, bool useQueryString = true)
+                where T : IDataTablesRequest, new()
+            {
+                const string baseUrl = "http://localhost/api/data/";
+
+                var request = new HttpRequestMessage();
+
+                var content = GetContent(data);
+
+                if (useQueryString)
+                {
+                    var urlBuilder = new StringBuilder(baseUrl + "?");
+                    foreach (var kv in content)
+                    {
+                        urlBuilder.Append(EncodeKeyValuePair(kv.Key, kv.Value) + "&");
+                    }
+
+                    request.RequestUri = new Uri(urlBuilder.ToString().TrimEnd('&'));
+                }
+                else
+                {
+                    request.RequestUri = new Uri(baseUrl);
+                    request.Content = new FormUrlEncodedContent(GetContent(data));
+                }
+
+                var controllerContext = new HttpControllerContext(new HttpConfiguration(), A.Fake<IHttpRouteData>(), request);
+                ActionContext = new HttpActionContext(controllerContext, A.Fake<HttpActionDescriptor>());
+
+                return new DataTablesWebApiBinder<T>();
+            }
+
+            public DefaultDataTablesRequest GetValidRequest()
+            {
+                return new DefaultDataTablesRequest
+                {
+                    Draw = 42,
+                    Start = 60,
+                    Length = 20,
+                    Search = new Search("foo", true),
+                    Columns = new ColumnCollection(new List<Column>
+                    {
+                        new Column("data", "name", true, true, "foo", true),
+                        new Column("data1", "name1", false, false, "", false)
+                    })
+                };
+            }
+
+            public ModelBindingContext GetBindingContext()
+            {
+                return new ModelBindingContext
+                {
+                    ModelMetadata = new ModelMetadata(new EmptyModelMetadataProvider(),
+                                                      null,
+                                                      null,
+                                                      typeof(DefaultDataTablesRequest),
+                                                      null)
+                };
+            }
+
+
+            private static IEnumerable<KeyValuePair<string, string>> GetContent(IDataTablesRequest data)
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("draw", data.Draw.ToString("d"));
+                dictionary.Add("start", data.Start.ToString("d"));
+                dictionary.Add("length", data.Length.ToString("d"));
+                dictionary.Add("search[value]", data.Search.Value);
+                dictionary.Add("search[regex]", data.Search.IsRegexValue.ToString().ToLower());
+
+                var idx = 0;
+                foreach (var column in data.Columns)
+                {
+                    var columnId = "columns[" + idx + "]";
+                    dictionary.Add(columnId + "[data]", column.Data);
+                    dictionary.Add(columnId + "[name]", column.Name);
+                    dictionary.Add(columnId + "[searchable]", column.Searchable.ToString().ToLower());
+                    dictionary.Add(columnId + "[orderable]", column.Orderable.ToString().ToLower());
+                    if (column.Search != null)
+                    {
+                        dictionary.Add(columnId + "[search][value]", column.Search.Value);
+                        dictionary.Add(columnId + "[search][regex]", column.Search.IsRegexValue.ToString().ToLower());
+                    }
+
+                    if (column.IsOrdered)
+                    {
+                        dictionary.Add("order[" + column.OrderNumber + "][column]", idx.ToString("d"));
+                        dictionary.Add("order[" + column.OrderNumber + "][dir]", column.SortDirection.ToString("d"));
+                    }
+                    ++idx;
+                }
+
+                return dictionary;
+            }
+
+            private static string EncodeKeyValuePair(string key, string value)
+            {
+                return HttpUtility.UrlEncode(key) + "=" + HttpUtility.UrlEncode(value ?? "");
+            }
+        }
+    }
+}
--- DataTables.WebApi/DataTablesWebApiBinder.cs
+++ DataTables.WebApi/DataTablesWebApiBinder.cs
@@ -42,35 +42,35 @@ namespace DataTables.WebApi
         /// <summary>
         /// Formatting to retrieve data for each column.
         /// </summary>
-        protected readonly string COLUMN_DATA_FORMATTING = "columns[{0}][data]";
+        protected readonly string COLUMN_DATA_FORMATTING = "columns[{0}].data";
         /// <summary>
         /// Formatting to retrieve name for each column.
         /// </summary>
-        protected readonly string COLUMN_NAME_FORMATTING = "columns[{0}][name]";
+        protected readonly string COLUMN_NAME_FORMATTING = "columns[{0}].name";
         /// <summary>
         /// Formatting to retrieve searchable indicator for each column.
         /// </summary>
-        protected readonly string COLUMN_SEARCHABLE_FORMATTING = "columns[{0}][searchable]";
+        protected readonly string COLUMN_SEARCHABLE_FORMATTING = "columns[{0}].searchable";
         /// <summary>
         /// Formatting to retrieve orderable indicator for each column.
         /// </summary>
-        protected readonly string COLUMN_ORDERABLE_FORMATTING = "columns[{0}][orderable]";
+        protected readonly string COLUMN_ORDERABLE_FORMATTING = "columns[{0}].orderable";
         /// <summary>
         /// Formatting to retrieve search value for each column.
         /// </summary>
-        protected readonly string COLUMN_SEARCH_VALUE_FORMATTING = "columns[{0}][search][value]";
+        protected readonly string COLUMN_SEARCH_VALUE_FORMATTING = "columns[{0}].search.value";
         /// <summary>
         /// Formatting to retrieve search regex indicator for each column.
         /// </summary>
-        protected readonly string COLUMN_SEARCH_REGEX_FORMATTING = "columns[{0}][search][regex]";
+        protected readonly string COLUMN_SEARCH_REGEX_FORMATTING = "columns[{0}].search.regex";
         /// <summary>
         /// Formatting to retrieve ordered columns.
         /// </summary>
-        protected readonly string ORDER_COLUMN_FORMATTING = "order[{0}][column]";
+        protected readonly string ORDER_COLUMN_FORMATTING = "order[{0}].column";
         /// <summary>
         /// Formatting to retrieve columns order direction.
         /// </summary>
-        protected readonly string ORDER_DIRECTION_FORMATTING = "order[{0}][dir]";
+        protected readonly string ORDER_DIRECTION_FORMATTING = "order[{0}].dir";
 
         /// <summary>
         /// Binds a new model with the DataTables request parameters.
@@ -114,8 +114,8 @@ namespace DataTables.WebApi
                 model.Length = Get<int>(requestParameters, "length");
 
                 // Populates the model with search (global search).
-                var searchValue = Get<string>(requestParameters, "search[value]");
-                var searchRegex = Get<bool>(requestParameters, "search[regex]");
+                var searchValue = Get<string>(requestParameters, "search.value");
+                var searchRegex = Get<bool>(requestParameters, "search.regex");
                 model.Search = new Search(searchValue, searchRegex);
 
                 // Get's the column collection from the request parameters.
